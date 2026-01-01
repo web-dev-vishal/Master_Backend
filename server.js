9b6d287a732d1bd67268552292e9b246715e8374
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-01 07:17:21 +0530

    commit

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 import express from "express";
+import "dotenv/config"
 const app = express();
+
+
+const PORT = process.env.PORT || 8000
+
+router.get('/', async (req, res) => {
+    return res.json({ message: "It's get request" });
+})
+
+app.listen(
+    PORT, () => console.log(`Server is running on PORT ${PORT}`)
+)
